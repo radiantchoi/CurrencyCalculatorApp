a7fa468f87ffe908c7db1587fbd5de2664871f75
Gordon Choi <thefirate@gmail.com>
2023-12-18 16:08:51 +0900

    test: 유즈케이스와 레포지토리에 대한 테스트 타겟 생성

---
+++ CurrencyCalculatorApp/CurrencyCalculatorAppTests/CurrencyRepositoryTests.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyRepositoryTests.swift
+//  CurrencyCalculatorAppTests
+//
+//  Created by Gordon Choi on 12/18/23.
+//
+
+import XCTest
+@testable import CurrencyCalculatorApp
+
+final class CurrencyRepositoryTests: XCTestCase {
+    var sut: CurrencyRepository!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        
+    }
+}
---
+++ CurrencyCalculatorApp/CurrencyCalculatorAppTests/CurrencyUseCaseTests.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyUseCaseTests.swift
+//  CurrencyCalculatorAppTests
+//
+//  Created by Gordon Choi on 12/18/23.
+//
+
+import XCTest
+@testable import CurrencyCalculatorApp
+
+final class CurrencyUseCaseTests: XCTestCase {
+    var sut: CurrencyUseCase!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        
+    }
+}
